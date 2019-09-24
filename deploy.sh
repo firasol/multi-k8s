5f572dcd162ca8d69ba58ee9620a716ded7ec27d
Firas Abughazaleh <firasabughazaleh@Firass-MacBook-Pro.local>
2019-09-24 10:35:01 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t fabughazaleh/multi-client:latest -t fabughazaleh/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t fabughazaleh/multi-server:latest -t fabughazaleh/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t fabughazaleh/multi-worker:latest -t fabughazaleh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push fabughazaleh/mulit-client:latest
+docker push fabughazaleh/mulit-server:latest
+docker push fabughazaleh/mulit-worker:latest
+docker push fabughazaleh/mulit-client:$SHA
+docker push fabughazaleh/mulit-server:$SHA
+docker push fabughazaleh/mulit-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=fabughazaleh/multi-server:$SHA
+kubectl set image deployments/client-deployment client=fabughazaleh/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=fabughazaleh/multi-worker:$SHA
