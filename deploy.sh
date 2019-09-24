2eace9b90e51c50e68a5255856f2c1f74b943988
Firas Abughazaleh <firasabughazaleh@Firass-MacBook-Pro.local>
2019-09-24 11:08:17 -0500

    fixed docker image names in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 docker build -t fabughazaleh/multi-client:latest -t fabughazaleh/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t fabughazaleh/multi-server:latest -t fabughazaleh/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t fabughazaleh/multi-worker:latest -t fabughazaleh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push fabughazaleh/mulit-client:latest
-docker push fabughazaleh/mulit-server:latest
-docker push fabughazaleh/mulit-worker:latest
-docker push fabughazaleh/mulit-client:$SHA
-docker push fabughazaleh/mulit-server:$SHA
-docker push fabughazaleh/mulit-worker:$SHA
+docker push fabughazaleh/multi-client:latest
+docker push fabughazaleh/multi-server:latest
+docker push fabughazaleh/multi-worker:latest
+docker push fabughazaleh/multi-client:$SHA
+docker push fabughazaleh/multi-server:$SHA
+docker push fabughazaleh/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=fabughazaleh/multi-server:$SHA
 kubectl set image deployments/client-deployment client=fabughazaleh/multi-client:$SHA
